6211e0d0023736eff8f450fdf752a1f1ce8a0463
Krisa Duwal <101305059+krisaduwal@users.noreply.github.com>
2022-11-28 16:48:41 +0545

    Update web.php

--- web.php
+++ web.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/', [HomeController::class, 'welcome'])->name('welcome')->middleware('auth');
+// Route::get('/', [HomeController::class, 'welcome'])->name('welcome')->middleware('auth');
 
 Route::get('/login', [HomeController::class, 'login'])->name('login');
 Route::get('/signup', [HomeController::class, 'signup'])->name('signup');
@@ -19,7 +19,7 @@ Route::post('/login', [UserController::class, 'loginUser'])->name('loginUser');
 Route::get('/logout', [UserController::class, 'logout'])->name('logout');
 
 
-Route::get('/', [ExpenseController::class, 'index']);
+Route::get('/', [ExpenseController::class, 'index'])->name('welcome')->middleware('auth');
 
 Route::post('/save-expense', [ExpenseController::class, 'saveExpense'])-> name('saveExpense');
 Route::post('/update-expense', [ExpenseController::class, 'updateExpense'])-> name('updateExpense');
